31c9f32e345c81777e3a6bf6b3bd605b22b65205
mkl680 <marumikael@gmail.com>
2026-03-19 21:52:07 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,11 @@
+import React from "react"
+import Link from "next/link"
 
+export default function Home() {
+  return (
+    <div>
+      <h1>TREGO</h1>
+      <Link href="/contact">Demo</Link>
+    </div>
+  )
+}
